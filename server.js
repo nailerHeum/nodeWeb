39758a656ecfa07a7e047c74332602d92f3f6a55
nailerHeum <kingman330@gmail.com>
2018-08-14 01:16:58 +0900

    server log & maintenance mode

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs'); 
 
 var app = express();
-
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    console.log('Unable to append to server.log.');
+  });
+  next(); // log는 유지보수 시에도 남길 수 있다. 
+})
+// maintenance mode (유지보수 시에 켜놓으면 유지보수 페이지만 대체됨)
+// next가 없기 때문에---------------------------------
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// }) 
+//-------------------------------------------------
 app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () =>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website!!',
-    currentYear: new Date().getFullYear()
+    
   });
 });
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    
   });
 });
 // / bad - send back json with errorMessage
